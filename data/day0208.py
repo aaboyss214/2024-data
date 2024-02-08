c98eee8bc1eaac7d676d3314dc82da18f3ce3b95
aaboyss214 <aaboyss214@naver.com>
2024-02-08 10:28:17 +0900

    v0.2

--- data/day0208.py
+++ data/day0208.py
@@ -17,4 +17,4 @@ def nCr(n, r) -> int:
 if __name__ == "__main__":
     n = int(input("Input n : "))
     r = int(input("Input r : "))
-    print(f"{n}C{r} = {nCr(n, r)}")
+    print(f"{n}C{r}  =  {nCr(n, r)}")
