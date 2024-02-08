22bb9415ed4505cc89184e58e99be5b652d21324
aaboyss214 <aaboyss214@naver.com>
2024-02-08 10:41:33 +0900

    v0.3 recursion preview

--- data/day0208.py
+++ data/day0208.py
@@ -1,8 +1,26 @@
-def factorial(number) -> int:
+'''def factorial(number) -> int:
+    """
+    factorial by repetition
+    :param number: number (int)
+    :return: factorial result (int)
+    """
     result = 1
     for i in range(1, number+1):
         result = result*i
     return result
+    '''
+
+def factorial(number) -> int:
+    """
+    factorial by recursion
+    :param number: number (int)
+    :return: factorial (int)
+    """
+    if number == 1:
+        return 1
+    else:
+        return number * factorial(number - 1)
+
 def nCr(n, r) -> int:
     """
     조합 함수
@@ -15,6 +33,9 @@ def nCr(n, r) -> int:
     return int(numerator / denominator)
 
 if __name__ == "__main__":
+    '''
     n = int(input("Input n : "))
     r = int(input("Input r : "))
-    print(f"{n}C{r}  =  {nCr(n, r)}")
+    print(f"{n}C{r}  =  {nCr(n, r)}")'''
+    f = int(input())
+    print(factorial(f))
