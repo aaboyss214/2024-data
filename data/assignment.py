81c02ce56dfa771a21f3dfdc525efd72199fb455
aaboyss214 <aaboyss214@naver.com>
2024-02-13 22:58:07 +0900

    3.2

--- data/assignment.py
+++ data/assignment.py
@@ -1,22 +1,40 @@
+## 함수 선언 부분 ## 
+def printPoly(t_x, p_x):
+    polyStr = "P(x) = "
+
+    for i in range(len(p_x)):
+        term = t_x[i]  # 항 차수
+        coef = p_x[i]  # 계수
+
+        if (coef >= 0):
+            polyStr += "+"
+        polyStr += str(coef) + "x^" + str(term) + " "
+
+    return polyStr
 
 
-arr = [('다현',200),('정연',150),('쯔위',90),('사나',30),('지효',15)]
+def calcPoly(xVal, t_x, p_x):
+    retValue = 0
 
+    for i in range(len(p_x)):
+        term = t_x[i]  # 항 차수
+        coef = p_x[i]  # 계수
+        retValue += coef * xValue ** term
+
+    return retValue
+
+
+## 전역 변수 선언 부분 ## 
+# tx = [300, 20, 0]
+# px = [7, -4, 5]
+tx_px = [[300, 20, 0], [7, -4, 5]]
+
+## 메인 코드 부분 ##
 if __name__ == "__main__":
-    while True:
-        new = input("추가할 친구-->")
-        chat = int(input("카톡 횟수-->"))
-        current = len(arr)-1
-        arr.append(None)
-        while True:
-            if arr[current][1]>chat:
-                arr.insert(current+1,(new,chat))
-                break
-            elif current == -1:
-                arr.insert(0,(new,chat))
-                break
-            else:
-                arr[current+1]=arr[current]
-                current = current -1
+    pStr = printPoly(tx_px[0], tx_px[1])
+    print(pStr)
 
-        print(arr)
+    xValue = int(input("X 값-->"))
+
+    pxValue = calcPoly(xValue, tx_px[0], tx_px[1])
+    print(pxValue)
