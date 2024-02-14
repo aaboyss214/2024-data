529276a49789dfe929021b190a96e2cb103ca13c
aaboyss214 <aaboyss214@naver.com>
2024-02-14 16:54:36 +0900

    Fibonacci using repetition

--- data/day0214.py
+++ data/day0214.py
@@ -1,13 +1,25 @@
-def decimal_to_octal(number: int) -> str:
-	"""
-	decimal number to octal number
-	:param number: integer (base dec)
-	:return: string (base octal)
-	"""
-	if number < 8:
-		return str(number)
-	else:
-		return decimal_to_octal(n//8) + str(n % 8)
+sum = 0;
+pre1 = 0
+pre2 = 1
+number = int(input())
 
-n = int(input('Input decimal number : '))
-print(decimal_to_octal(n))
+print(pre2, end = ' ')
+while number >= 2:
+	sum = pre1 + pre2
+	number = number - 1
+	print(sum, end=' ')
+	pre1=pre2
+	pre2=sum
+
+# def fibo(n) :
+# 	if n == 0 :
+# 		return 0
+# 	elif n == 1 :
+# 		return 1
+# 	else :
+# 		return fibo(n-1) + fibo(n-2)
+# print()
+# number = int(input())
+# print('피보나치 수 --> 0 1 ', end='')
+# for i in range(2, number):
+# 	print(fibo(i), end=' ')
