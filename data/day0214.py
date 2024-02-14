b49061f0d6e50ed3c46db4e1b4134b1579e015a3
aaboyss214 <aaboyss214@naver.com>
2024-02-14 17:30:20 +0900

    Fibonacci using memo

--- data/day0214.py
+++ data/day0214.py
@@ -1,25 +1,50 @@
-sum = 0;
-pre1 = 0
-pre2 = 1
-number = int(input())
+# sum = 0
+# pre1 = 0
+# pre2 = 1
+# number = int(input())
+# print(pre2, end = ' ')
+# while number >= 2:
+# 	sum = pre1 + pre2
+# 	number = number - 1
+# 	print(sum, end=' ')
+# 	pre1=pre2
+# 	pre2=sum
 
-print(pre2, end = ' ')
-while number >= 2:
-	sum = pre1 + pre2
-	number = number - 1
-	print(sum, end=' ')
-	pre1=pre2
-	pre2=sum
-
-# def fibo(n) :
-# 	if n == 0 :
-# 		return 0
-# 	elif n == 1 :
-# 		return 1
-# 	else :
-# 		return fibo(n-1) + fibo(n-2)
+def fibo(n) :
+	if n == 0 :
+		return 0
+	elif n == 1 :
+		return 1
+	else :
+		return fibo(n-1) + fibo(n-2)
 # print()
 # number = int(input())
 # print('피보나치 수 --> 0 1 ', end='')
 # for i in range(2, number):
 # 	print(fibo(i), end=' ')
+
+
+memo = [None for _ in range(100)]#컴프리헨션
+
+def fibo_memo(n: int, memo: list) -> int:
+	if memo[n] is not None:
+		return memo[n] #한번 계산한 값을 사용하는 것
+	if n<2:
+		result = n
+	else:
+		result = fibo_memo(n-1, memo) + fibo_memo(n-2, memo)
+		memo[n]=result
+	return result
+
+number = int(input())
+for i in range(0,number):
+	print(i,end = ' ')
+	print(fibo_memo(i,memo))
+
+
+
+
+
+
+
+
