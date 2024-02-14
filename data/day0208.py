f12216a1586b02edef7453d341464832d99051b8
aaboyss214 <aaboyss214@naver.com>
2024-02-14 11:21:20 +0900

    graph v0.1

--- data/day0208.py
+++ data/day0208.py
@@ -9,10 +9,10 @@ G1, G3 = None, None
 
 ## 메인 코드 부분 ##
 G1 = Graph(4)
-G1.graph[0][1] = 1; G1.graph[0][2] = 1; G1.graph[0][3] = 1
-G1.graph[1][0] = 1; G1.graph[1][2] = 1
-G1.graph[2][0] = 1; G1.graph[2][1] = 1; G1.graph[2][3] = 1
-G1.graph[3][0] = 1; G1.graph[3][2] = 1
+G1.graph[0][3] = 1
+G1.graph[1][2] = 1; G1.graph[1][3] = 1
+G1.graph[2][1] = 1
+G1.graph[3][0] = 1; G1.graph[3][1] = 1
 
 
 print('## G1 무방향 그래프 ##')
@@ -21,13 +21,13 @@ for row in range(G1.SIZE) :
 		print(G1.graph[row][col], end=' ')
 	print() #줄바꿈
 
-
-G3 = Graph(4)
-G3.graph[0][1] = 1; G3.graph[0][2] = 1
-G3.graph[3][0] = 1; G3.graph[3][2] = 1
-
-print('## G3 방향 그래프 ##')
-for row in range(G3.SIZE) :
-	for col in range(G3.SIZE) :
-		print(G3.graph[row][col], end=' ')
-	print()
+#
+# G3 = Graph(4)
+# G3.graph[0][1] = 1; G3.graph[0][2] = 1
+# G3.graph[3][0] = 1; G3.graph[3][2] = 1
+#
+# print('## G3 방향 그래프 ##')
+# for row in range(G3.SIZE) :
+# 	for col in range(G3.SIZE) :
+# 		print(G3.graph[row][col], end=' ')
+# 	print()
