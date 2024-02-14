314075f7fc83fe0710431b628f3ecfdef7610949
aaboyss214 <aaboyss214@naver.com>
2024-02-14 14:33:50 +0900

    graph0.4

--- data/graph.py
+++ data/graph.py
@@ -1,18 +1,19 @@
+def dfs(g, i, visited):
+    visited[i] = 1
+    print(arr[i], end =' ')
+    for j in range(len(g)):
+        if g[i][j] == 1 and not visited[j]:
+            dfs(g, j, visited)
+
 graph = [
-    [0,0,1,1,0],
-    [0,0,1,0,0],
-    [1,1,0,1,1],
-    [1,0,1,0,0],
-    [0,0,1,0,0],
+    [0,1,1,0,0,0],
+    [1,0,0,1,0,0],
+    [1,0,0,1,0,0],
+    [0,1,1,0,1,1],
+    [0,0,0,1,0,1],
+    [0,0,0,1,1,0]
 ]
+arr = ['문별','솔라','휘인','쯔위','선미','화사']
 
-def dfs(g, v, visited):
-    visited[v] = True
-    print(chr(ord('A') +v), end = ' ')
-    for i in range(len(g)):
-        if g[v][i] == True and not visited[i]:
-            dfs(g, i, visited)
-
-
-visited = [False]*len(graph)
+visited = [0]*len(graph)
 dfs(graph, 0, visited)
