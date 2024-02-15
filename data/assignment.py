622c424f8d9fde4c0c2163d134cf6de9d3ebde10
aaboyss214 <aaboyss214@naver.com>
2024-02-16 02:45:16 +0900

    7.1

--- data/assignment.py
+++ data/assignment.py
@@ -1,73 +1,63 @@
 ## 함수 선언 부분 ##
-def isStackFull() :
-	global SIZE, stack, top
-	if (top >= SIZE-1) :
+import random
+def isQueueFull() :
+	global SIZE, queue, front, rear
+	if (rear == SIZE-1) :
 		return True
 	else :
 		return False
 
-def isStackEmpty() :
-	global SIZE, stack, top
-	if (top == -1) :
+def isQueueEmpty() :
+	global SIZE, queue, front, rear
+	if (front == rear) :
 		return True
 	else :
 		return False
 
-def push(data) :
-	global SIZE, stack, top
-	if (isStackFull()) :
+def enQueue(data) :
+	global SIZE, queue, front, rear
+	if (isQueueFull()) :
+		print("큐가 꽉 찼습니다.")
 		return
-	top += 1
-	stack[top] = data
+	rear += 1
+	queue[rear] = data
 
-def pop() :
-	global SIZE, stack, top
-	if (isStackEmpty()) :
+def deQueue() :
+	global SIZE, queue, front, rear
+	if (isQueueEmpty()) :
+		print("큐가 비었습니다.")
 		return None
-	data = stack[top]
-	stack[top] = None
-	top -= 1
+	front += 1
+	data = queue[front]
+	queue[front] = None
+
+	for i in range(front + 1, rear + 1): 	# 모든 사람을 한칸씩 앞으로 당긴다.
+		queue[i - 1] = queue[i]
+		queue[i] = None
+	front = -1
+	rear -= 1
+
 	return data
 
 def peek() :
-	global SIZE, stack, top
-	if (isStackEmpty()) :
+	global SIZE, queue, front, rear
+	if (isQueueEmpty()) :
+		print("큐가 비었습니다.")
 		return None
-	return stack[top]
+	return queue[front+1]
 
 ## 전역 변수 선언 부분 ##
-SIZE = 100
-stack = [ None for _ in range(SIZE) ]
-top = -1
+SIZE = 5
+queue = [ None for _ in range(SIZE) ]
+front = rear = -1
 
-## 메인 코드 부분 ##
-if __name__ == "__main__" :
+if __name__=="__main__":
+	arr = ['정국','뷔','지민','진','슈가']
+	random.shuffle(arr)
+	for i in range(len(arr)):
+		enQueue(arr[i])
 
-	with open("진달래꽃.txt", 'r', encoding='UTF8') as rfp :
-		lineAry = rfp.readlines()
-
-	print("----- 원본 -----")
-	for line in lineAry :
-		push(line)
-		print(line, end = '')
-	print()
-
-	print("----- 거꾸로 처리된 결과 -----")
-	while True :
-		line = pop()
-		if line == None :
-			break
-
-		miniStack = [None for _ in range(len(line))]
-		miniTop = -1
-
-		for ch in line :
-			miniTop += 1
-			miniStack[miniTop] = ch
-
-		while True :
-			if miniTop == -1 :
-				break
-			ch = miniStack[miniTop]
-			miniTop -= 1
-			print(ch, end = ' ')
+	while rear != -1:
+		print("대기 줄 상태: ",queue)
+		print(deQueue(),'님 식당에 들어감')
+	print('식당 영업 종료!')
