c1a10ee53cd0ec8de2c8905605c05fe72e831d03
aaboyss214 <aaboyss214@naver.com>
2024-02-16 13:42:31 +0900

    baekjun

--- data/day0214.py
+++ data/day0214.py
@@ -1,50 +1,37 @@
-sum = 0
-pre1 = 0
-pre2 = 1
-number = int(input())
-print(pre2, end = ' ')
-while number >= 2:
-	sum = pre1 + pre2
-	number = number - 1
-	print(sum, end=' ')
-	pre1=pre2
-	pre2=sum
+from collections import deque
 
-def fibo(n) :
-	if n == 0 :
-		return 0
-	elif n == 1 :
-		return 1
-	else :
-		return fibo(n-1) + fibo(n-2)
-# print()
-# number = int(input())
-# print('피보나치 수 --> 0 1 ', end='')
-# for i in range(2, number):
-# 	print(fibo(i), end=' ')
-print()
-number = int(input())
-memo = [None for _ in range(number)]#컴프리헨션
+# input_data = list(map(int, input().split()))
+# print(input_data)
+# n=input_data[0]
+# m=input_data[1]
+# k=input_data[2]
+# x=input_data[3]
+# print(n, m, k, x)
 
-def fibo_memo(n: int, memo: list) -> int:
-	if memo[n] is not None:
-		return memo[n] #한번 계산한 값을 사용하는 것
-	if n<2:
-		result = n
-	else:
-		result = fibo_memo(n-1, memo) + fibo_memo(n-2, memo)
-		memo[n]=result
-	return result
+n, m, k, x = map(int, input().split())
+# print(n, m, k, x)
+g=[[] for _ in range(n+1)]
 
+for _ in range(m):
+    start, end = map(int, input().split())
+    g[start].append(end)
 
-for i in range(0,number):
-	print(i,end = ' ')
-	print(fibo_memo(i,memo))
+d = [-1]*(n+1)
+d[x] = 0
 
+q=deque([x])
+while q:
+    now = q.popleft()
+    for next in g[now]:
+        if d[next] == -1:
+            d[next] = d[now] + 1
+            q.append(next)
 
+check = False
+for i in range(1, n+1):
+    if d[i] == k:
+        print(i)
+        check = True
 
-
-
-
-
-
+if check == False:
+    print(-1)
