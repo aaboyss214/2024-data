e69511d73eb8eef8941e98970ac530e48f947f25
aaboyss214 <aaboyss214@naver.com>
2024-02-19 22:35:02 +0900

    0219.2

--- data/coding test.py
+++ data/coding test.py
@@ -1,16 +1,14 @@
+test_case = int(input())
 
-repetition= int(input())
-a=[]
-
-for i in range(repetition):
-    a.append(list(map(int, input().split())))
-    max_num=max(a[i])
-    min_num=min(a[i])
-    a[i].remove(max_num)
-    a[i].remove(min_num)
-
-for j in range(repetition):
-    sum = 0
-    for k in a[j]:
-        sum = sum + k
-    print(f'#{j+1} {round(sum/len(a[j]))}')
+grade = ['A+','A0','A-','B+','B0','B-','C+','C0','C-','D0',]
+for i in range(test_case):
+    student_number, student = map(int, input().split())
+    arr1 = []
+    for j in range(student_number):
+        a,b,c = map(int , input().split())
+        arr1.append(a*0.35+b*0.45+c*0.2)
+    scores = arr1[student-1]
+    arr1.sort(reverse=True)
+    index_ = arr1.index(scores)
+    n = int(student_number/10)
+    print(f'#{i+1} {grade[index_//n]}')
