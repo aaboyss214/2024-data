72b1b68cdd4ec97d2440579f203894f0b3740e02
aaboyss214 <aaboyss214@naver.com>
2024-02-20 18:52:59 +0900

    0220.1

--- data/coding test.py
+++ data/coding test.py
@@ -1,9 +1,20 @@
 T=int(input())
 for test_case in range(1, T+1):
-    n, k = map(int, input().split())
-    arr = [input().split() for _ in range(n)]
-    row = '0'.join([''.join(i) for i in arr])
-    column = '0'.join(''.join(i) for i in list(zip(*arr)))
-    row_sum = sum([True for i in row.split('0') if i == '1'*k])
-    column_sum = sum([True for i in column.split('0') if i == '1'*k])
-    print(f'#{test_case} {row_sum + column_sum}')
+    hour1, minute1, hour2, minute2 = map(int, input().split())
+    hour, minute = 0, 0
+    if minute1 + minute2 >= 60:
+        minute = minute1 + + minute2 - 60
+        hour = hour + 1
+    else:
+        minute = minute1 + + minute2
+    if hour1 + hour2 > 12:
+        hour = hour1 + hour2 - 12
+    else:
+        hour = hour1 + hour2
+
+    if minute1 + minute2 >= 60:
+        minute = minute1 + + minute2 - 60
+        hour = hour + 1
+    else:
+        minute = minute1 + + minute2
+    print(f'#{test_case} {hour} {minute}')
