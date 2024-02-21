151f28ef969baf0f9c4b053f517c5dd3085eca0e
aaboyss214 <aaboyss214@naver.com>
2024-02-21 16:21:43 +0900

    0221.1

--- data/coding test.py
+++ data/coding test.py
@@ -1,7 +1,11 @@
 T = int(input())
 for test_case in range(1,T+1):
-    len_ = int(input())
-    numbers = list(map(int, input().split()))
-    numbers.sort()
-    print(f'#{test_case}', end=' ')
-    print(*numbers)
+    money = int(input())
+    answer=[0]*8
+    charge=[50000,10000,5000,1000,500,100,50,10]
+    for i in range(8):
+    if money//charge[i] > 0:
+    answer[i] = money//charge[i]
+    money = money%charge[i]
+    print(f'#{test_case}')
+    print(*answer)
