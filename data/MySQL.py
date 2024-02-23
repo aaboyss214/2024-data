eebad999f44a226a9741d5e7ae6d6bc9f656b204
aaboyss214 <aaboyss214@naver.com>
2024-02-23 15:48:18 +0900

    0223.1

--- data/MySQL.py
+++ data/MySQL.py
@@ -2,47 +2,31 @@ import pymysql
 
 print('hello')
 
-def get_book_by_name(pbookname):
-    #데이터베이스 접속 관련 변수들 초기화
-    host = "localhost"
-    port = 3306
-    database = "madang"
-    username = "root"
-    password = "7345"
-    #접속 상태 확인
-    conflag = True
-    #A처리 블락 = 파이썬 예외파트에 나오나봄
+def get_total():
+    connection = pymysql.connect(host='localhost',  # MySQL 서버 주소 (예: 'localhost')
+                                 user='root',  # MySQL 사용자 이름
+                                 password='7345',  # MySQL 비밀번호
+                                 database='madang',  # 사용할 데이터베이스 이름
+                                 cursorclass=pymysql.cursors.DictCursor)
+
     try:
-        print('데이터베이스 연결 준비...')
-        conn = pymysql.connect(host=host, user=username, password=password, db=database, port=port, charset='utf8')
-    #mysql에 접속하는 함수, utf-8은 한글때문에(오른쪽 아래보면 파이썬도 인코딩 버전 이것이다.). 위변수가 일치하면 conn가 True 받는다.
-        print('데이터베이스 연결 성공')
-    except Exception as err:#try 블럭에서 예외발생이 여기로 온다.
-        print('데이터베이스 연결 실패:')
-        conflag = False
+        with connection.cursor() as cursor:
+            # SQL 쿼리 실행
+            sql = """
+               SELECT   SUM(saleprice) AS total
+               FROM   Orders od
+               WHERE   EXISTS (SELECT *
+                       FROM   Customer cs
+                       WHERE   address LIKE '%대한민국%' AND 
+                       cs.custid=od.custid);
+               """
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            print(f"총 판매 가격: {result['total']}")
 
-    #접속에 성공한다면 쿼리문 실행 코드 실행
-    if conflag == True:#커서 객체 생성 cursor
-        cursor = conn.cursor()
-        sqlString = 'select * from book where bookname = %s;'
-        res = cursor.execute(sqlString, pbookname)
-        data = cursor.fetchall()
-        print('data의 타입=' , type(data))
-
-        print('{0}\t{1:<} \t{2:<} \t{3:>}'.format('bookid','bookname','publisher','price'))
-
-        #레코드들 출력
-        for rowdata in data:
-            if rowdata[3] == None:
-                print('{0}\t{1:<} \t{2:<} \t{3:>}'.format(rowdata[0], rowdata[1], rowdata[2], 0))
-            else:
-                print('{0}\t{1:<} \t{2:<} \t{3:>}'.format(rowdata[0], rowdata[1], rowdata[2], rowdata[3]))
-
-        cursor.close()
-        conn.close()
+    finally:
+        connection.close()
 
 
 if __name__=="__main__":
-
-    bookname = input('원하는 bookname를 입력 =>')
-    get_book_by_name(bookname)
+    get_total()
