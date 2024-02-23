d0c94033ed804cd38bda9b4b24616efc626f0f27
aaboyss214 <aaboyss214@naver.com>
2024-02-23 18:21:48 +0900

    0223.1

--- data/coding test.py
+++ data/coding test.py
@@ -1,12 +1,18 @@
-def day_count(mounth):
-    total = 0
-    day_count_arr = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    for i in day_count_arr[0:mounth-1]:
-        total = total + i
-    return total
+T = int(input())
+for test_case in range(1, T + 1):
+    number_alp = int(input())
+    arr = []
+    count = 0
+    for _ in range(number_alp):
+        alp , n = map(str, input().split())
+        arr.append(alp)
+        arr.append(int(n))
+    print(f'#{test_case}')
+    for i in range(number_alp):
 
-if __name__=="__main__":
-    T = int(input())
-    for test_case in range(1, T + 1):
-        m1, d1, m2, d2 = map(int, input().split())
-        print(f'#{test_case} {day_count(m2) + d2 - (day_count(m1) + d1)+1}')
+        for _ in range(arr[2*i +1]):
+            print(arr[2*i],end='')
+            count = count +1
+            if count % 10 == 0:
+                print()
+    print()
