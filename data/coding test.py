2fd00f1e4ba211414bec7021673c2138b50b5b85
aaboyss214 <aaboyss214@naver.com>
2024-03-05 20:37:53 +0900

    0305

--- data/coding test.py
+++ data/coding test.py
@@ -1,26 +1,14 @@
 T = int(input())
 
-arr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-
 for test_case in range(1, T+1):
-    str_ = input()
-    bin_str = str()
-    temp = str()
-    ascii = str()
-    for i in str_:
-        binary = bin(arr.index(i))
-        bin_str += "0"*(8 -len(binary)) + binary[2:]
-
-    for j in bin_str:
-        temp += j
-        if len(temp)%8 == 0:
-            ascii += chr(int(temp,2))
-            temp = str()
-    print(f'#{test_case} {ascii}')
-
-
-
-
-
-
-
+    arr = []
+    count = 2
+    number = input()
+    number2 = number
+    while len(arr) != 10:
+        for i in range(len(number2)):
+            if int(number2[i]) not in set(arr):
+                arr.append(int(number2[i]))
+        number2 = str(int(number) * count)
+        count = count + 1
+    print(f'#{test_case} {(count-2)*int(number)}')
