9f9779d9a7137054e4ebead5f0b10707873a22c3
aaboyss214 <aaboyss214@naver.com>
2024-03-13 13:41:25 +0900

    0313

--- data/coding test.py
+++ data/coding test.py
@@ -1,25 +1,47 @@
+# T = int(input())
+# for test_case in range(1, T+1):
+#     number, count = map(int, input().split())
+#     arr = list(i for i in str(number))
+#     max = max(arr)
+#     if arr.count(max) > 1:
+#         pass
+#     else:
+#         index = arr.index(max)
+#         if count > 1:
 
-for test_case in range(1, 11):
+#         else:
+#             arr[0], arr[index] = arr[index], arr[0]
+#     print(arr)
+
+# max값 새 배열에 pop 하고 나머지는 그냥 배열에 붙이면 되잖아
+# 1. max값이 중복되어있으면 제일 뒤에있는 수를  pop해야하는데
+# 2. arr.pop(원하는위치) >> reverse.arr 해서 index 찾아서 하면 될듯
+# 이렇게 하는게 아니다 요소를 교환해야해서 a, b = b, a 써야하나...... 흠..
+
+def dfs(cnt):
+    global ans
+    if cnt == n:
+        ans = max(ans, int("".join(map(str, lst))))
+        return
+ 
+    for i in range(Length - 1):
+        for j in range(i + 1, Length):
+            lst[i], lst[j] = lst[j], lst[i]
+            chk = int("".join(map(str, lst)))
+            if (cnt, chk) not in visited:
+                dfs(cnt + 1)
+                visited.append((cnt, chk))
+            lst[i], lst[j] = lst[j], lst[i]
+
+if __name__=="__main__": 
     T = int(input())
-    arr = list(map(int, input().split()))
-    count = 0
-    for i in range(2, T-2):
-        a, b, c, d = arr[i-2], arr[i-1], arr[i+1], arr[i+2]
-        if arr[i] > a and arr[i] > b and arr[i] > c and arr[i] > d:
-            if a >= b:
-                max1 = a
-            else:
-                max1 = b 
-
-            if c >= d:
-                max2 = c
-            else:
-                max2 = d
-
-            if max1 > max2:
-                max = max1
-            else:
-                max = max2
-            
-            count = count + arr[i] - max
-    print(f'#{test_case} {count}')
+    
+    for i in range(1, T + 1):
+        num, n = input().split()
+        n = int(n)
+        lst = list(map(int, num))
+        Length = len(lst)
+        visited = []
+        ans = 0
+        dfs(0)
+        print(f'#{i} {ans}')
---
+++ data/datastructure.py
@@ -0,0 +1,50 @@
+# password = input()
+# confirmedPassword = 'aBc'
+
+# arePassSame = password == confirmedPassword
+
+# print(arePassSame)
+
+# x = 5
+# y = 1
+# result = x < y
+# print(result)
+
+# arr='Abc'
+# print((arr.lower()))
+
+# if int(input()) > 1:
+#     print(1 )
+# else:
+#     print(-1)
+
+# def get_country():
+#     country = input('Please enter a country name: ')
+#     return country
+
+# def print_in_console(country, capital):
+#     print('The caital of {} is {}'.format(country, capital))
+
+# def country_to_capital():
+#     country = get_country()
+
+#     capital = ''
+
+#     if country == 'France':
+#         capital = 'Paris'
+#     elif country == 'Germany':
+#         capital = 'Berlin'
+#     elif country == 'Itary':
+#         capital = 'Rome'
+
+#     print_in_console(country, capital)
+
+# country_to_capital()
+
+# dict = {'a':'apple', 'b':'base', 'c':'cap'}
+# print(dict.items())
+# for i in dict.items():
+#     print(i)
+
+arr = [1,2,3]
+print([*arr,1,2])
