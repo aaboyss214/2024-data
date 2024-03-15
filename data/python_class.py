fb4c60d0ed1f009f2bb583e183f58071992547b3
aaboyss214 <156396768+aaboyss214@users.noreply.github.com>
2024-03-15 13:26:03 +0900

    0315

--- data/python_class.py
+++ data/python_class.py
@@ -1,7 +1,15 @@
-a = input('정수를 입력하세요: ')
-print(type(a))
-if int(a) %2 == 0:
-    print('짝수입니다.')
-else:
-    print('홀수입니다.')
-print(type(int(a)))
+print('==========================')
+list1 = list(map(int, input().split()))
+avg = sum(list1)/3
+var = 0
+for i in list1:
+    var = var + pow(i-avg,2)
+var = var / 3
+std = var**(1/2)
+print('==========================')
+print('===파이썬 중간고사 결과===')
+print('==========================')
+print('전체 수강생 수 : ', 3)
+print('평균 : ', avg)
+print('분산 : ', var)
+print('표준편차 : ', std)
