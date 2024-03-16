143396baca0dfa6094bacfc2e774c47969864f20
aaboyss214 <aaboyss214@naver.com>
2024-03-16 15:17:39 +0900

    0316.2

--- data/coding test.py
+++ data/coding test.py
@@ -1,20 +1,45 @@
-# 4로나누면 1 = 1
-# 4로나누면 2 = 2
-# 4로나누면 3 = 3
-# 4로나누면 0 = 4
-# 위에건 행
-# 아래는 열
-# 4로나누면 몫이 0
-# 4번재행만 몫이 나머지에 비해 +1
-a, b = map(int, input().split())
+n = int(input())
+for i in range(n*2-1):
+    if i < n:
+        print(' '*(n-1-i)+'*'*(2*i+1))
+    else:
+        print(' '*(i-n+1)+'*'*(2*(2*(n-1)-i)+1))
 
-a_ = [a//4, a%4]
-b_ = [b//4, b%4]
-if a % 4 == 0:
-    a_[0] -= 1
-    a_[1] += 4
-if b % 4 == 0:
-    b_[0] -= 1
-    b_[1] += 4
-print(abs(a_[0]-b_[0])+abs(a_[1]-b_[1]))
+# n = int(input())
 
+# temp = n-1
+# stars = 1
+# for i in range(n-1):
+#     print(' '*temp+'*'*stars)
+#     temp -= 1
+#     stars += 2
+
+# for i in range(n):
+#     print(' '*temp+'*'*stars)
+#     temp += 1
+#     stars -= 2
+
+# a, b = map(int,input().split())
+# #5 3
+# p = 0
+# count = 0
+# x,y = 0,0#x = 좌우, y = 상하
+
+# for i in range(a*b):
+#     if p%4 == 0:#오른쪽
+#         x += 1
+#         if x==b:
+#             count += 1
+#             p=1
+#             x=0
+#     elif p%4 == 1:#아래
+#         y += 1
+#         if y==a:
+#             count += 1
+#             p=2
+#             x=0
+            
+#     elif p%4 == 2:#왼쪽
+        
+#     elif p%4 == 3:#위로
+#         pass
