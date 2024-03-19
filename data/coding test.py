2689c401aeeebf960587e153aab8fa1443137d30
aaboyss214 <aaboyss214@naver.com>
2024-03-19 17:58:41 +0900

    0319

--- data/coding test.py
+++ data/coding test.py
@@ -1,45 +1,13 @@
-n = int(input())
-for i in range(n*2-1):
-    if i < n:
-        print(' '*(n-1-i)+'*'*(2*i+1))
-    else:
-        print(' '*(i-n+1)+'*'*(2*(2*(n-1)-i)+1))
+def fac(a):
+    data = 1
+    for i in range(1,a+1):
+        data = data*i
+    return data
 
-# n = int(input())
+def comb(n , m):
+    return int(fac(m)/(fac(m-n)*fac(n)))
 
-# temp = n-1
-# stars = 1
-# for i in range(n-1):
-#     print(' '*temp+'*'*stars)
-#     temp -= 1
-#     stars += 2
+if __name__=='__main__':   
+    n, m = map(int, input().split())
+    print(comb(n , m))
 
-# for i in range(n):
-#     print(' '*temp+'*'*stars)
-#     temp += 1
-#     stars -= 2
-
-# a, b = map(int,input().split())
-# #5 3
-# p = 0
-# count = 0
-# x,y = 0,0#x = 좌우, y = 상하
-
-# for i in range(a*b):
-#     if p%4 == 0:#오른쪽
-#         x += 1
-#         if x==b:
-#             count += 1
-#             p=1
-#             x=0
-#     elif p%4 == 1:#아래
-#         y += 1
-#         if y==a:
-#             count += 1
-#             p=2
-#             x=0
-            
-#     elif p%4 == 2:#왼쪽
-        
-#     elif p%4 == 3:#위로
-#         pass
