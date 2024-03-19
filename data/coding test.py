09c89b8501f399fdb3e0f30e91ff5a9cb7af9c8a
aaboyss214 <aaboyss214@naver.com>
2024-03-19 18:17:17 +0900

    0319.1

--- data/coding test.py
+++ data/coding test.py
@@ -1,13 +1,7 @@
-def fac(a):
-    data = 1
-    for i in range(1,a+1):
-        data = data*i
-    return data
+# 대칭차집합은 전체 집합에서 교집합 뺀것
+# 대칭차집합 원소 개수 출력
 
-def comb(n , m):
-    return int(fac(m)/(fac(m-n)*fac(n)))
-
-if __name__=='__main__':   
-    n, m = map(int, input().split())
-    print(comb(n , m))
-
+a, b = map(int, input().split())
+arr_a = set(map(int, input().split()))
+arr_b = set(map(int, input().split()))
+print((arr_a|arr_b)-(arr_a&arr_b))
