878bc8dfe5dd97ff66fbce0f8ea4a2738b1d03cf
aaboyss214 <aaboyss214@naver.com>
2024-03-26 17:16:17 +0900

    0326

--- data/coding test.py
+++ data/coding test.py
@@ -1,27 +1,9 @@
-#11111111111111111111111111111111
-# input()
-# list1 = input().split()
-# input()
-# list2 = input().split()
-# print(' '.join([ str(list1.count(a)) for a in list2 ]))
-
-#22222222222222222222222222222222
-# from collections import Counter
-
-# input()
-# list1 = input().split()
-# input()
-# list2 = input().split()
-# counter = Counter(list1)
-# print(' '.join(str(counter[a]) for a in list2))
-
-#33333333333333333333333333333333
-# input()
-# list1 = input().split()
-# input()
-# list2 = input().split()
-# counts = {}
-# for item in list1:
-#     counts[item] = counts.get(item, 0) + 1
-# print(' '.join(str(counts.get(item, 0)) for item in list2))
-
+test_case = int(input())
+arr1 = list(map(int,input().split()))
+arr2 = list(map(int,input().split()))
+arr1.sort()
+arr2.sort(reverse = -1)
+sum = 0
+for i in range(test_case):
+    sum += arr1[i]*arr2[i]
+print(sum)
