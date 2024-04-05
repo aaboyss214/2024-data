e5ec5fd62aef8f0428d15cc3b6868d6298597dd9
aaboyss214 <aaboyss214@naver.com>
2024-04-05 14:29:57 +0900

    0405 one more

--- data/python_class.py
+++ data/python_class.py
@@ -1,18 +1,15 @@
-<<<<<<< HEAD
-test_case = int(input())
-arr1 = list(map(int,input().split()))
-arr2 = list(map(int,input().split()))
-arr1.sort()
-arr2.sort(reverse = -1)
-sum = 0
-for i in range(test_case):
-    sum += arr1[i]*arr2[i]
-print(sum)
-=======
-s = input('이진수를 입력하세요: ')
-_10 = 0
-for i in range(len(s)):
-    if s[i:i+1] == '1':
-       _10 += pow(2,len(s)-1-i)
-print(_10)
->>>>>>> 4fdb824800988f7f0f4da369d00b5fadffbbd0bd
+number = int(input())
+arr = [[] for _ in range(number)]
+
+for i in range(number):
+    for j in range(number*i+1,(number*(i+1))+1):
+        arr[i].append(j)
+
+for i in range(number):
+    if i % 2 ==0:
+        pass
+    else:
+        arr[i].reverse()
+    for j in range(number):
+        print(' '*(3-len(str(arr[i][j]))),arr[i][j],end='')
+    print()
